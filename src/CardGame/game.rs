4635651ba87ca6b815e47bd2e32d0ac9f3631226
NilsMichael <nfi005@uib.no>
2023-07-13 11:59:51 +0200

    Added circular list and the HOF list to the struct

--- src/CardGame/game.rs
+++ src/CardGame/game.rs
@@ -1,3 +1,4 @@
+use crate::CircularList::circular_list::CircularList;
 use crate::feature::Feature;
 use super::player::Player;
 use super::player::Move;
@@ -14,7 +15,10 @@ pub enum GameState {
     End
 }
 
-pub struct Game {
+pub struct Game<F> 
+where
+    F: Fn(Game<F>, Card) -> bool
+{
     game_name: String,
     state: GameState,
     player_moves: Vec<(Move, bool)>,
@@ -22,19 +26,19 @@ pub struct Game {
     discard: Vec<Card>,
     deck: Vec<Card>,
     pile: Vec<(Card, Option<Card>)>,
-    players: Vec<Player>,
+    players: CircularList<Player>,
     rules: Vec<(Feature, Vec<Expr>)>,
     actions: Vec<(GameState, Vec<Expr>)>,
-    can_place_card: Vec<String>
+    can_place_card: Vec<F>
 }
 
 
-impl Game {
-    pub fn new(name: String, plrs: Vec<String>) -> Game {
+impl<F: Fn(Game<F>, Card) -> bool> Game<F> {
+    pub fn new(game_name: String, players: Vec<Player>) -> Game<F> {
         Game {
-            game_name: name,
+            game_name,
             state: GameState::Start,
-            players: Vec::new(),
+            players: CircularList::from_list(players),
             player_moves: Vec::new(),
             card_gen: Vec::new(),
             discard: Vec::new(),
